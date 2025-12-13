c3be333106694c7cacf7450661a5418fbfe3975b
Sai Praveen J <spjonnal@uwaterloo.ca>
2025-12-12 19:43:46 -0500

    client recent changes

--- client/src/moviesearch.js
+++ client/src/moviesearch.js
@@ -14,19 +14,47 @@ function MovieSearch(){
     const [contribute, setContribute] = useState(false);
     const [sortkey, setSortKey] = useState("")
     const [loading, setLoading] = useState(false);
+    const[closeGenreSelectData,setCloseGenreSelectData] = useState(false);
+    const [closeDataInfo, setCloseDataInfo] = useState(false);
     const [typeHead, setTypeHead] = useState("");
     const [suggestions, setSuggestions] = useState([]);
     const [movie_info, setMovieInfo] = useState([]);
+    const [postMovieData, setPostMovieData] = useState({
+        movie_name: "",
+        release_date:"",
+        movie_duration: "",
+        imdb_rating: "",
+        language: "",
+        cast_and_crew: "",
+        certificates: "def",
+        genres: [],
+        dubbing: "",
+    });
+    
     
 
     const handleCheckBoxChange = (chgeEvent) => {
         setContribute(chgeEvent.target.checked);
     };
 
+    const handleSetCloseMovieInfo = ()=> setCloseDataInfo(!closeDataInfo);
+    
+
     const handleGenreSelection = (event)=>{
         
         setSelectedGenre(event.value);
         
+    };
+    const handleCloseGenreSelectData = ()=>setCloseGenreSelectData(!closeGenreSelectData);
+
+    const handleContributedGenreSelection = (event)=>{
+        setPostMovieData({...postMovieData,genres:event.value,});
+    }
+
+    const handleContributedRestData = (e)=>{
+        setPostMovieData({
+            ...postMovieData,[e.target.name]: e.target.value,
+        });
     };
 
     const handleTypeHead = async (event) => {
@@ -34,7 +62,7 @@ function MovieSearch(){
         
         setTypeHead(inputText);
 
-        if (inputText.length < 2) {
+        if (inputText.length <=1) {
             setSuggestions([]);
             return;
         }
@@ -47,7 +75,7 @@ function MovieSearch(){
             });
 
             const data = await response.json();
-
+            
             setSuggestions(data.return_data || []);
         } catch (err) {
             console.error("React fetch error:", err);
@@ -57,7 +85,7 @@ function MovieSearch(){
     const handleMovieClick = async (title)=>{
         
         const selected_movie = title;
-        console.log("selected movie name = ",selected_movie);
+        
         try{
             const movie_complete_info = await fetch("http://localhost:4001/api/movieinfo",{
                 method :"POST",
@@ -109,9 +137,30 @@ function MovieSearch(){
         }   
     };
 
-    const displayGrace = (event) => {
-        event.preventDefault();
-        alert("Movie data submitted! (You can now link this to a backend)");
+    const displayGrace = async (event) => {
+        
+        try{
+            
+            
+        
+            const send_contribution_data = await fetch("http://localhost:4001/api/send-contribution-data",{
+                method:"POST",
+                headers:{
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify(postMovieData )
+            })
+            
+            if(send_contribution_data.status === 200){
+                alert("Data inserted. We appreciate your contribution..");
+            }
+        }
+        catch(error){
+            console.log("some error while storing your information",error.toString());
+    
+        }
+    
+        
     };
     const sorting = [...resp].sort((a,b)=>{
         if(!sortkey){
@@ -121,6 +170,7 @@ function MovieSearch(){
             return parseFloat(b[sortkey]) - parseFloat(a[sortkey]); // descending order of IMDB ID
         }
         else if(sortkey === "Release Date"){
+        
             const yearA = parseInt(a[sortkey].slice(0,4),10);
             const yearB = parseInt(b[sortkey].slice(0,4),10);
             return yearB-yearA; // descending order of release date
@@ -136,7 +186,7 @@ function MovieSearch(){
                     <h1>Welcome to Mowickie... One stop destination for best movie recommendations!</h1>
                    
                     
-                    <SelectGenre onChange = {handleGenreSelection} value={selectedGenre}></SelectGenre>
+                    <SelectGenre onChange = {handleGenreSelection} value={selectedGenre} id = "fav_genre_select"></SelectGenre>
                     <br /><br />
                     <div>
                         <label htmlFor='typehead'></label>
@@ -167,28 +217,38 @@ function MovieSearch(){
                     <button id = "submit_button" type='submit'>Submit</button>
                 </form>
                 {movie_info.length > 0 && (
-                    <table >
-                        <thead>
-                            <tr>
-                                {Object.keys(movie_info[0]).map((col_name, key) => (
-                                    <th key={key}>{col_name}</th>
-                                ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {movie_info.map((mov, ind) => (
-                                <tr key={ind}>
-                                    {Object.entries(mov).map(([key,val], i) => (
-                                        <td key={i}>{
-                                            key.toLowerCase().includes("url") && typeof val ==="string" && val.startsWith("http")?
-                                            ( <a href={val} target="_blank" rel="noreferrer">{val}</a>  ):(val)
-                                            }
-                                        </td>
+                    
+                        !closeDataInfo && (
+                            <>
+                            <button type='button' id = "cancel_movie_data" onClick={handleSetCloseMovieInfo}>X</button>
+                        
+                            
+                            <table >
+                                <thead>
+                                    <tr>
+                                        {Object.keys(movie_info[0]).map((col_name, key) => (
+                                            <th key={key}>{col_name}</th>
+                                        ))}
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {movie_info.map((mov, ind) => (
+                                        <tr key={ind}>
+                                            {Object.entries(mov).map(([key,val], i) => (
+                                                <td key={i}>{
+                                                    key.toLowerCase().includes("url") && typeof val ==="string" && val.startsWith("http")?
+                                                    ( <a href={val} target="_blank" rel="noreferrer">{val}</a>  ):(val)
+                                                    }
+                                                </td>
+                                            ))}
+                                        </tr>
                                     ))}
-                                </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                                </tbody>
+                            </table>
+                            </>
+                        )
+                        
+                    
                 )}
                 {
                     loading && (
@@ -197,38 +257,50 @@ function MovieSearch(){
                 }
                 {
                     !loading && resp.length>0 && (
-                    <div>
-                        <select onChange ={(e)=>setSortKey(e.target.value)}>
-                            <option value="">--select--</option>
-                            <option value="Ratings">IMDB Ratings</option>
-                            <option value="Release Date">Release Date</option>
-                        </select>
-                    </div>
+                        !closeDataInfo &&(
+                            <div >
+                                <select onChange ={(e)=>setSortKey(e.target.value)} id='sort_genre_data'>
+                                    <option value="">--select--</option>
+                                    <option value="Ratings">IMDB Ratings</option>
+                                    <option value="Release Date">Release Date</option>
+                                </select>
+                            </div>
+                        )
                     )
                 }
                 {sorting.length > 0 && (
-                    <table >
-                        <thead>
-                            <tr>
-                                {Object.keys(sorting[0]).map((col_name, key) => (
-                                    <th key={key}>{col_name}</th>
-                                ))}
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {sorting.map((mov, ind) => (
-                                <tr key={ind}>
-                                    {Object.entries(mov).map(([key,val], i) => (
-                                        <td key={i}>{
-                                            key.toLowerCase().includes("url") && typeof val ==="string" && val.startsWith("http")?
-                                            ( <a href={val} target="_blank" rel="noreferrer">{val}</a>  ):(val)
-                                            }
-                                        </td>
+                    !closeDataInfo && (
+                        <>
+                        <button type='button' id = 'cancel_movie_data' onClick={handleSetCloseMovieInfo}>X</button>
+                        <table >
+                            <thead>
+                                <tr>
+                                    {Object.keys(sorting[0]).map((col_name, key) => (
+                                        <th key={key}>{col_name}</th>
                                     ))}
                                 </tr>
-                            ))}
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                {sorting.map((mov, ind) => (
+                                    
+                                        
+                                            
+                                    <tr key={ind}>
+                                        {Object.entries(mov).map(([key,val], i) => (
+                                            <td key={i}>{
+                                                key.toLowerCase().includes("url") && typeof val ==="string" && val.startsWith("http")?
+                                                ( <a href={val} target="_blank" rel="noreferrer">{val}</a>  ):(val)
+                                                }
+                                            </td>
+                                        ))}
+                                    </tr>
+                                        
+                                    
+                                ))}
+                            </tbody>
+                        </table>
+                        </>
+                    )
                 )}
                 <br/>
                 <br/>
@@ -245,28 +317,34 @@ function MovieSearch(){
                     <form onSubmit={displayGrace}>
                         <br />
                         <div className='grid_display'>
-                            <label htmlFor = 'movie_name'> Enter movie/series name here : 
-                            <input type='text' name = 'movie_name' placeholder='Movie/Series name' id = 'movie_name' required /> 
+                            <label htmlFor = 'movie_name' id = "contribution_fields">Enter movie/series name here: 
+                            <input type='text' name = 'movie_name' placeholder='Movie/Series name' id = 'movie_name' required onChange={handleContributedRestData} /> 
                             </label>
                             <br /> 
-                            <label htmlFor = 'movie_duration'>Enter the movie duration here :
-                            <input type="text" name='movie_duration' inputMode="decimal" pattern="[0-9]*[.,]?[0-9]*" placeholder='Duration (minutes)' id = 'movie_duration' required />
+                            <label htmlFor = 'release_date' id = "contribution_fields">Enter/select movie release date : 
+                            <input type='date' name = 'release_date' placeholder='select release date' id = 'release_date' required onChange={handleContributedRestData} /> 
+                            </label>
+                            <br/>
+                            <label htmlFor = 'movie_duration' id = "contribution_fields">Enter the movie duration here :
+                            <input type="text" name='movie_duration' inputMode="decimal" pattern="[0-9]*[.,]?[0-9]*" placeholder='Duration (minutes)' id = 'movie_duration' required 
+                                onChange={handleContributedRestData}
+                            />
                             </label>
                             <br />
-                            <label htmlFor = 'imdb rating'>Enter the IMDB ratings of the movie here :
-                            <input type='text' name='imdb rating' placeholder='IMDB Ratings' id = 'imdb rating'required />
+                            <label htmlFor = 'imdb rating' id = "contribution_fields">Enter the IMDB ratings of the movie here :
+                            <input type='text' name='imdb_rating' placeholder='IMDB Ratings' id = 'imdb_rating'required onChange={handleContributedRestData}/>
                             </label>
                             <br />
-                            <label htmlFor = 'language'>Enter the original language of the movie here :
-                            <input type='text' name='language' placeholder='Original Language' id ='language' required />
+                            <label htmlFor = 'language' id = "contribution_fields">Enter the original language of the movie here :
+                            <input type='text' name='language' placeholder='Original Language' id ='language' required onChange={handleContributedRestData}/>
                             </label>
                             <br />
-                            <label htmlFor = 'cast and crew'>Enter cast and crew inhtmlFormation here :
-                            <input type='text' name = 'cast and crew' placeholder='Cast & Crew Info' id ='cast and crew' />
+                            <label htmlFor = 'cast and crew' id = "contribution_fields">Enter cast and crew inhtmlFormation here :
+                            <input type='text' name = 'cast_and_crew' placeholder='Cast & Crew Info' id ='cast_and_crew' onChange={handleContributedRestData}/>
                             </label>
                             <br />
-                            <label htmlFor='certificates' className='censorship_dropdown'>Select the censorship of the movie:</label>
-                            <select name='certificates' id = 'certificates' >
+                            <label htmlFor='certificates' className='censorship_dropdown' id = "contribution_fields">Select the censorship of the movie:</label>
+                            <select name='certificates' id = 'certificates' onChange={handleContributedRestData} >
                                 <option value='def'>-- Please Select --</option>
                                 <option value='U'>Universal Rating (U)</option>
                                 <option value='U/A'>Universal with Parental Guidance (U/A)</option>
@@ -280,14 +358,14 @@ function MovieSearch(){
                                 <option value='Nota'>None of the above</option>
                             </select>
                             <br />
-                            <label htmlFor = 'genres' className='genre_design'>Select the movie genre from below :</label>
-                            <SelectGenre name='genres' id= 'genres' onSubmit></SelectGenre>
+                            <label htmlFor = 'genres' className='genre_design' id = "contribution_fields">Select the movie genre from below :</label>
+                            <SelectGenre name='genres' id= 'genres' onSubmit onChange={handleContributedGenreSelection}></SelectGenre>
                             <p>*Hold down the Ctrl (windows) or Command (Mac) button to select multiple options.</p>
                             <br/>
-                            <label htmlFor = 'dubbing'>Enter all langauges in which the moies is available here :</label>
-                            <input type='text' name  = 'dubbing' placeholder='Dubbed Languages' id = 'dubbing' />
+                            <label htmlFor = 'dubbing'  id = "contribution_fields"> Enter all langauges in which the moies is available here :</label>
+                            <input type='text' name  = 'dubbing' placeholder='Dubbed Languages' id = 'dubbing' onChange={handleContributedRestData}/>
                             <br />
-                            <button id = "submit_button" type='submit'>Submit Your Movie</button>
+                            <button id = "submit_button" type='submit'><p id = "contribution_fields">Submit Your Movie</p></button>
                         </div>
                         
                     </form>
