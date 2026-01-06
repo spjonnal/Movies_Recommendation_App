9e8f2ac3f1310e99af0b0cc3c7e427907a936dfb
spjonnal <138257312+spjonnal@users.noreply.github.com>
2026-01-05 22:25:01 -0500

    Update index.js

--- server_duplicate/index.js
+++ server_duplicate/index.js
@@ -101,6 +101,8 @@ app.post('/api/movieinfo',async(req,res)=>{
   try{
     const {movie_name} = req.body;
     const movie_title = movie_name.title
+    console.log("type head movie in node = ",movie_name,movie_title);
+    
    // const db = db_connection();
     //const complete_movie_info = await specificMovie(db,movie_name.selected_movie);
     const complete_movie_info = await specificMovie(movie_title);
