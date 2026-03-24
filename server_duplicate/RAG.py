f07d7efe3839b490461599c3fe47bc85e070236e
spjonnal <138257312+spjonnal@users.noreply.github.com>
2026-03-23 23:50:34 -0400

    Update RAG.py

--- server_duplicate/RAG.py
+++ server_duplicate/RAG.py
@@ -169,7 +169,7 @@ async def ask_llm(request: ConversationRequest):
 
 if __name__ == "__main__":
     print("in the main python")
-    uvicorn.run("RAG:app", host="0.0.0.0", port=os.getenv("PORT"))
+    uvicorn.run("RAG:app", host="0.0.0.0", port=8000)
 #     #uvicorn.run("RAG:app", host="0.0.0.0", port=8000,reload=True)
     
 
