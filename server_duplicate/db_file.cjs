542427ff4cd34b84ac2d7cdc470dd106c5b341e2
spjonnal <138257312+spjonnal@users.noreply.github.com>
2026-04-05 12:36:13 -0400

    Update db_file.cjs

--- server_duplicate/db_file.cjs
+++ server_duplicate/db_file.cjs
@@ -209,7 +209,7 @@ async function typeHeadSearch(query) {
             `,[`%${query}%`]
 
         )
-        retrurn response.rows;
+        return response.rows;
     }    
     catch(err){
         throw err;
