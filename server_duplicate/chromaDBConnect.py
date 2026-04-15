20c27474d63d8a2e9fadd9c5bb5d5453634a703a
spjonnal <138257312+spjonnal@users.noreply.github.com>
2026-04-14 21:16:42 -0400

    Refactor DB connection and enhance vector search
    Refactor database connection to use connection pooling and improve resource management. Update genre loading and vector search functionality.

--- server_duplicate/chromaDBConnect.py
+++ server_duplicate/chromaDBConnect.py
@@ -1,12 +1,13 @@
 import json
 import chromadb
 import psycopg2
+from psycopg2 import pool
 import uvicorn
 import os, sys, time
 from dotenv import load_dotenv
 from functools import lru_cache
 from fastapi import FastAPI
-
+from contextlib import asynccontextmanager
 vector_search = FastAPI()
 
 client = chromadb.Client(
@@ -17,110 +18,145 @@ client = chromadb.Client(
 
 #load_dotenv("pg_admin4_connect_for_py.env")
 
-@vector_search.get('/active')
-async def health_chec():
-    return {"status":'active'}
-
-# ---------------- DB CONNECTION ---------------- #
-def dbConn():
-    db_connection = psycopg2.connect(
-        host=os.getenv("DB_HOST"),
+# pooled connection to overcome random sql connection shutdown
+connection_pool = pool.SimpleConnectionPool(
+        1,10,host=os.getenv("DB_HOST"),
         port=os.getenv("DB_PORT"),
         database=os.getenv("DB_NAME"),
         user=os.getenv("DB_USER"),
         password=os.getenv("DB_PASSWORD"),
     )
-    return db_connection, db_connection.cursor()
-
-conn,cursor = dbConn()
+# ---------------- DB CONNECTION ---------------- #
+def dbConn():
+    conn = connection_pool.getconn()
+    cursor = conn.cursor()
+    return conn, cursor
+def release_conn(conn):
+    connection_pool.putconn(conn)
 
 # ---------------- CHROMA INIT ---------------- #
+collection = None
 
+def get_collection():
+    global collection
+    if collection is None:
+        collection = client.get_or_create_collection("movie_information")
+    return collection
 
-collection = client.get_or_create_collection("movie_information")
 
 # ---------------- LOAD GENRES ONLY ONCE ---------------- #
 def load_genres_once():
-    if collection.count() > 0:
+    
+    col = get_collection()
+
+    if col.count() > 0:
         return  # Avoid duplicate embeddings
 
-    cursor.execute("""
-        SELECT DISTINCT genres 
-        FROM movie_information 
-        WHERE genres IS NOT NULL;
-    """)
-    
-    genres = [row[0] for row in cursor.fetchall()]
+    conn,cursor = dbConn()
+    try:
+        cursor.execute("""
+            SELECT DISTINCT genres 
+            FROM movie_information 
+            WHERE genres IS NOT NULL;
+        """)
+        
+        genres = [row[0] for row in cursor.fetchall()]
 
-    collection.add(
-        ids=[str(i) for i in range(len(genres))],
-        documents=genres,
-        metadatas=[{"genre": g} for g in genres]
-    )
+        col.add(
+            ids=[str(i) for i in range(len(genres))],
+            documents=genres,
+            metadatas=[{"genre": g} for g in genres]
+        )
+    finally:
+        release_conn(conn=conn)
 
-load_genres_once()
+# to improve vector search by reducing cold start
+@asynccontextmanager
+async def startup(app :FastAPI):
+    try:
+        print("Starting system warmup")
+        conn,cursor = dbConn()
+        load_genres_once()
+
+        # Warm vector DB
+        vector_collection = get_collection()
+        vector_collection.query(
+            query_texts=['any genre'],
+            n_results=1,
+            include=['metadatas']
+        )
+
+        # warm up SQL DB
+        cursor.execute("SELECT 1;")
+        release_conn(conn = conn)
+
+        print("system ready")
+        yield# The application serves requests here
+    except Exception as e:
+        print("some error while warm up = ",e)
+        yield# The application serves requests here
+        
+vector_search = FastAPI(lifespan=startup)
+
+@vector_search.get('/active')
+async def health_chec():
+    return {"status":'active'}
 
 # ---------------- CACHED QUERY ---------------- #
 #@lru_cache(maxsize=50)
 @vector_search.get("/movie_search")
 async def vector_data_retrieval(query:str):
-    # -------- VECTOR SEARCH -------- #
-    output = collection.query(
-        query_texts=[query],
-        n_results=5,
-        include=["metadatas"]
-    )
-
-    resulting_genres = [m['genre'] for m in output['metadatas'][0]]
-
-    if not resulting_genres:
-        return []
-
-    # -------- SQL QUERY -------- #
-    placeholders = " OR ".join(["genres ILIKE %s"] * len(resulting_genres))
-
-    sql_query = f"""
-        SELECT 
-            *
-        FROM movie_information
-        WHERE ratings >= 3
-        AND ({placeholders})
-        LIMIT 50;
-    """
-
-    params = [f"%{g}%" for g in resulting_genres]
-
-    cursor.execute(sql_query, params)
-    rows = cursor.fetchall()
     
-    # -------- FAST PARSING -------- #
-    results = [
-        {
-            'Certificate': r[0] or "False",
-            'IMDB ID': r[1],
-            'Overview': r[2] if r[2] else "Not Available",
-            'Release Date': r[3] if r[3] else "Not Available",
-            'Runtime': r[4],
-            'Title': r[5] if r[5] else "Not Available",
-            'Ratings': r[6],
-            'Genres': r[7] if r[7] else "Not Available",
-            'Available Languages': r[8] if r[8] else "Not Available",
-            'Cast and Crew': r[9] if r[9] else "Not Available",
-            'Youtube Trailer Link': r[10] if r[10] else "Not Available"
-        }
-        for r in rows
-    ]
+    conn, cursor = dbConn()
+    try:
+        # -------- VECTOR SEARCH -------- #
+        output = collection.query(
+            query_texts=[query],
+            n_results=5,
+            include=["metadatas"]
+        )
 
-    
+        resulting_genres = [m['genre'] for m in output['metadatas'][0]]
 
-    #print(f" Total Time: {end - start:.3f}s")
+        if not resulting_genres:
+            return []
+
+        # -------- SQL QUERY -------- #
+        placeholders = " OR ".join(["genres ILIKE %s"] * len(resulting_genres))
+
+        sql_query = f"""
+            SELECT 
+                *
+            FROM movie_information
+            WHERE ratings >= 3
+            AND ({placeholders})
+            LIMIT 50;
+        """
+
+        params = [f"%{g}%" for g in resulting_genres]
+
+        cursor.execute(sql_query, params)
+        rows = cursor.fetchall()
+        
+        # -------- FAST PARSING -------- #
+        results = [
+            {
+                'Certificate': r[0] or "False",
+                'IMDB ID': r[1],
+                'Overview': r[2] if r[2] else "Not Available",
+                'Release Date': r[3] if r[3] else "Not Available",
+                'Runtime': r[4],
+                'Title': r[5] if r[5] else "Not Available",
+                'Ratings': r[6],
+                'Genres': r[7] if r[7] else "Not Available",
+                'Available Languages': r[8] if r[8] else "Not Available",
+                'Cast and Crew': r[9] if r[9] else "Not Available",
+                'Youtube Trailer Link': r[10]
+            }
+            for r in rows
+        ]
+    finally:
+        release_conn(conn)
     return results
-
-
-# ---------------- MAIN ---------------- #
-#if __name__ == "__main__":
-    #query = sys.argv[1]
-    #data = vector_data_retrieval(query)
-    #print(json.dumps(data, indent=2, default=str))
     
     
